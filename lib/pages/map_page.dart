6f7019baa7b2428bfd974066f42394dc97400ab5
Selim Kaelin <73212781+sekael@users.noreply.github.com>
2026-02-27 21:07:56 +0100

    feat: improve feedback possibilities (#19)
    * feat: improve feedback options for non-github users
    
    * chore: rename template
    
    * chore: remove app version from feedback form

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:touringbuddy_frontend/components/error_snackbar.dart';
+import 'package:touringbuddy_frontend/components/text/body.dart';
 import 'package:touringbuddy_frontend/components/text/button.dart';
+import 'package:touringbuddy_frontend/core/config/theme.dart';
 import 'package:touringbuddy_frontend/core/logging/app_logger.dart';
 import 'package:touringbuddy_frontend/features/map/data/map_view_model.dart';
 import 'package:touringbuddy_frontend/features/map/presentation/location_picker.dart';
@@ -33,17 +35,64 @@ class _MapPageState extends State<MapPage> {
     super.dispose();
   }
 
-  Future<void> _launchFeedbackUrl() async {
+  void _showFeedbackOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(
+            touringBuddySpacings.lg,
+          ), // Using your theme spacings
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    _launchGithubIssue();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                    elevation: 4,
+                  ),
+                  child: ButtonText(
+                    buttonText: 'Open Issue on GitHub',
+                    styleOverride: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: touringBuddySpacings.md),
+              CenterAlignedBodyText(
+                bodyText:
+                    "If you don't have a GitHub account, you can always reach out to us at feedback@tourenbuddy.ch",
+              ),
+              SizedBox(height: touringBuddySpacings.lg),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _launchGithubIssue() async {
     final Uri url = Uri.parse(
       'https://github.com/sekael/touringbuddy_frontend/issues/new?template=beta_feedback.yml',
     );
 
-    logger.i('Launching feedback URL: $url');
+    logger.i('Launching Github issue URL: $url');
 
     if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
       if (mounted) {
         rootMessengerKey.currentState?.showSnackBar(
-          ErrorSnackbar(message: 'Could not open feedback page').build(),
+          ErrorSnackbar(message: 'Could not open Github issues page').build(),
         );
       }
     }
@@ -64,7 +113,7 @@ class _MapPageState extends State<MapPage> {
                 vertical: 8.0,
               ),
               child: ElevatedButton(
-                onPressed: _launchFeedbackUrl,
+                onPressed: () => _showFeedbackOptions(context),
                 style: ElevatedButton.styleFrom(
                   // You can customize colors here to make it stand out
                   elevation: 2,
