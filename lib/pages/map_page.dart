05f6f9ebf96b2627404760ecd0fc15e7e2adb22d
Selim Kälin <selim.kaelin@ipt.ch>
2026-02-27 20:37:46 +0100

    feat: add feedback button and issue template

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:touringbuddy_frontend/components/error_snackbar.dart';
+import 'package:touringbuddy_frontend/components/text/button.dart';
+import 'package:touringbuddy_frontend/core/logging/app_logger.dart';
 import 'package:touringbuddy_frontend/features/map/data/map_view_model.dart';
 import 'package:touringbuddy_frontend/features/map/presentation/location_picker.dart';
 import 'package:touringbuddy_frontend/features/map/presentation/map_action_overlay.dart';
 import 'package:touringbuddy_frontend/features/map/presentation/touringbuddy_map.dart';
 import 'package:touringbuddy_frontend/features/tours/map/tours_marker_layer.dart';
+import 'package:touringbuddy_frontend/main.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -15,6 +20,9 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   late final ToursMarkerLayer _toursSymbolLayer;
+
+  final String _githubUsername = 'sekael';
+  final String _repoName = 'touringbuddy_frontend';
 
   @override
   void initState() {
@@ -28,6 +36,22 @@ class _MapPageState extends State<MapPage> {
     super.dispose();
   }
 
+  Future<void> _launchFeedbackUrl() async {
+    final Uri url = Uri.parse(
+      'https://github.com/sekael/touringbuddy_frontend/issues/new?template=beta_feedback.yml',
+    );
+
+    logger.i('Launching feedback URL: $url');
+
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      if (mounted) {
+        rootMessengerKey.currentState?.showSnackBar(
+          ErrorSnackbar(message: 'Could not open feedback page').build(),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -36,6 +60,22 @@ class _MapPageState extends State<MapPage> {
         appBar: AppBar(
           title: const Text('Touring Buddy'),
           automaticallyImplyLeading: false,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+                vertical: 8.0,
+              ),
+              child: ElevatedButton(
+                onPressed: _launchFeedbackUrl,
+                style: ElevatedButton.styleFrom(
+                  // You can customize colors here to make it stand out
+                  elevation: 2,
+                ),
+                child: const ButtonText(buttonText: 'Feedback'), //
+              ),
+            ),
+          ],
         ),
         body: Stack(
           children: [TouringBuddyMap(), MapActionOverlay(), LocationPicker()],
