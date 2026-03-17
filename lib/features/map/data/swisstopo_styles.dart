c16bf85900d8c90b6ee200a36abc146c7915c5fc
Selim Kaelin <73212781+sekael@users.noreply.github.com>
2026-03-17 06:15:39 +0100

    feat: initialize swisstopo style at startup (#41)

--- lib/features/map/data/swisstopo_styles.dart
+++ lib/features/map/data/swisstopo_styles.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/services.dart';
 import 'package:touringbuddy_frontend/features/map/domain/base_map_style.dart';
 
 class SwisstopoStyles {
   static const String base =
       'https://vectortiles.geo.admin.ch/styles/ch.swisstopo.basemap.vt/style.json';
-  static const String fullColor = 'assets/swisstopo_wmts_style.json';
+  static String _fullColorJson = '';
+  static String get fullColor => _fullColorJson;
 
   static final List<BaseMapStyle> all = [
     const BaseMapStyle('Base', base),
-    const BaseMapStyle('Full Color', fullColor),
+    BaseMapStyle('Full Color', _fullColorJson),
   ];
+
+  static Future<void> initialize() async {
+    _fullColorJson = await rootBundle.loadString(
+      'assets/swisstopo_wmts_style.json',
+    );
+  }
 }
